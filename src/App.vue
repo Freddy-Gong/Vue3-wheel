4739ef896066fe5b280f161af9ed9c6aae20ef95
Freedy <21914103@zju.edu.cn>
2020-09-16 16:31:16 +0800

    用vite创建项目，并且手动初始化Vue-router

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-link to="/">Home</router-link>|
+  <router-link to="Doc">Doc</router-link>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
