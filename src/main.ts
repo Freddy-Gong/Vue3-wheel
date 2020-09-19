7a1b5fb5c1c794a40cc27416e0e55d7814373819
Freedy <21914103@zju.edu.cn>
2020-09-19 15:41:47 +0800

    加载中

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import './lib/gulu.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
-import './lib/gulu.scss'
 import './index.scss'
 import { router } from './router'
 const app = createApp(App)
