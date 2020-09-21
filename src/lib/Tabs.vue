d192aad4fbafeb9077a3635ccb9f3ac1b58c564b
Freedy <21914103@zju.edu.cn>
2020-09-21 14:33:07 +0800

    创建Tabs和Tab组件

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>xxx</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Tab from "./Tab.vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang='scss' scoped>
+</style>
--- src/views/DOC/TabsDemo.vue
+++ src/views/DOC/TabsDemo.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>xxxx</div>
+  <div>
+    <h1>示例1</h1>
+    <Tabs>
+      <Tab title="标题1">内容1</Tab>
+      <Tab title="标题2">内容2</Tab>
+    </Tabs>
+  </div>
 </template>
 
 <script>
-export default {};
+import Tabs from "../../lib/Tabs.vue";
+import Tab from "../../lib/Tab.vue";
+export default {
+  components: { Tabs, Tab },
+};
 </script>
 
 <style lang="scss" scoped>
