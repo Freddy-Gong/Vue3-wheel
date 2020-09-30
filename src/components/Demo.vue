b77d5313451557a9a2b2ddb6d09e1c5b129a882c
Freedy <21914103@zju.edu.cn>
2020-09-30 15:03:52 +0800

    创建Demo组件

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,34 +1,41 @@
 <template>
-  <div>
-    <h1>Switch 组件示例</h1>
-    <Demo :component="Switch1Demo" />
-    <Demo :component="Switch2Demo" />
+  <div class="demo">
+    <h2>{{ component.__sourceCodeTitle }}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre
+        class="language-html"
+        v-html="
+          Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')
+        "
+      />
+    </div>
   </div>
 </template>
 
-<script lang='ts'>
-import { ref } from "vue";
-import Demo from "../../components/Demo.vue";
-import Button from "../../lib/Button.vue";
-import Switch1Demo from "../../components/Switch1.demo.vue";
-import Switch2Demo from "../../components/Switch2.demo.vue";
+<script lang="ts">
+import { defineComponent } from "vue";
 import "prismjs";
 import "prismjs/themes/prism-okaidia.css";
+import Button from "../lib/Button.vue";
 const Prism = (window as any).Prism;
 
-export default {
+export default defineComponent({
+  props: {
+    component: Object,
+  },
   components: {
     Button,
-    Demo,
   },
   setup() {
-    return {
-      Switch1Demo,
-      Switch2Demo,
-      Prism,
-    };
+    return { Prism };
   },
-};
+});
 </script>
 
 <style lang="scss" scoped>
