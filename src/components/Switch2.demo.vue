97b69de62097d972c43ae498d8c98b81adb11e11
Freedy <21914103@zju.edu.cn>
2020-09-30 14:41:10 +0800

    展示源代码

---
+++ src/components/Switch2.demo.vue
@@ -0,0 +1,23 @@
+<demo>disable 功能</demo>
+
+<template>
+  <Switch v-model:value="bool" disabled />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Switch from "../lib/Switch.vue";
+import { ref } from "vue";
+export default {
+  components: {
+    Switch,
+  },
+  setup() {
+    const bool = ref(false);
+    return { bool };
+    return {
+      bool,
+    };
+  },
+};
+</script>
